dbac45c9bf35d22b545db8e3a3e33186c8c670af
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-10-06 16:42:24 +0000

    started putting in questions

--- game.py
+++ game.py
@@ -4,3 +4,72 @@ def main():
     print(f"Welcome, {user_name}!")
 
 main()
+
+trivia1 = [
+    {
+        'question': 'the first avenger',
+        'answer': 'captian america',
+    },
+    {
+        'question': 'ragnarok',
+        'answer': 'thor',
+    },
+    {
+        'question': 'the first avenger',
+        'answer': 'captian america',
+    },
+    {
+        'question': 'the incredible',
+        'answer': 'captian america',
+    },
+    {
+        'question': 'the wasp',
+        'answer': 'antman',
+    }
+]
+
+trivia2 = [
+    {
+        'question': 'AIF',
+        'answer': 'avengers infinity war',
+    },
+    {
+        'question': 'CA TWS',
+        'answer': 'captain american the winter soldier',
+    },
+    {
+        'question': 'DP&WV',
+        'answer': 'deadpool and wolverine',
+    },
+    {
+        'question': 'GOTG',
+        'answer': 'guardians of the galaxy',
+    },
+    {
+        'question': 'TB TNA',
+        'answer': 'thunderbolts the new avengers',
+    }
+]
+
+trivia3 = [
+    {
+        'question': '__a__ _a____e_',
+        'answer': 'black panther',
+    },
+    {
+        'question': '_o_i',
+        'answer': 'loki',
+    },
+    {
+        'question': '__a___e_ _i___',
+        'answer': 'scarlet witch',
+    },
+    {
+        'question': '',
+        'answer': 'captian amermaica',
+    },
+    {
+        'question': 'the wasp',
+        'answer': 'captian marvel',
+    }
+]
